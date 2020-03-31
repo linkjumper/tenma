94326a6e03871228e75d699f666e50b50beed1b3
michael glembotzki <michael.glembotzki@irisgmbh.de>
2020-03-31 08:40:46 +0200

    Confirm new voltage/current settings

--- tenma.py
+++ tenma.py
@@ -3,6 +3,7 @@
 import tenma.module as tenma
 import argparse
 import signal
+import sys
 
 parser = argparse.ArgumentParser(description='Setup control for Tenma Power Supply')
 parser.add_argument('-v', '--voltage', help='set voltage', required=True, type=float)
@@ -13,9 +14,17 @@ def signal_handler(sig, frame):
     print('Terminate Tenma control')
 signal.signal(signal.SIGINT, signal_handler)
 
+def confirmSetup(voltage_new, voltage_old, current_new, current_old):
+    if (current_old != current_new or voltage_old != voltage_new):
+        answer = input(f'Setup {voltage_new:2.5} V {current_new:2.5} A correct? [yN]').lower()
+        if (answer != 'y'):
+            sys.exit()
+
 def main():
     args = vars(parser.parse_args())
     power_supply = tenma.Tenma(args['device'])
+    confirmSetup(args['voltage'], power_supply.getVoltage(),
+                 args['current'], power_supply.getCurrent())
     power_supply.setVoltage(args['voltage'])
     power_supply.setCurrent(args['current'])
     power_supply.printInfo()
